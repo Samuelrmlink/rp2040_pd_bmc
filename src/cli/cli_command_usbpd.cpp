c9a98e9ebc903b2269f4f9c04b6d4490ef85b6d5
Samuel Hedrick <samuel@rml.ink>
2024-08-19 04:43:50 -0700

    ..

--- src/cli/cli_command_usbpd.cpp
+++ src/cli/cli_command_usbpd.cpp
@@ -4,14 +4,49 @@ typedef struct {
     void (*const fn)(Cli *cli, std::vector<std::string>& argv);
     const char *name;
 } CliUsbpdSubcommand;
+typedef struct {
+    void (*const cb)(Cli *cli);
+    const char *option;
+} CliUsbpdShowOption;
 
 
-void cli_usbpd_show(Cli *cli, std::vector<std::string>& argv) {
-    cli_printf(cli, "<show some data here>" EOL);
+void cli_usbpd_show_pdo(Cli *cli) {
+    cli_printf(cli, "Show PDO here..." EOL);
 }
+
+// This function is part of the 'show' subcommand - so while it does indeed 
+// 'show help' - it only does so for the 'show' subcommand of the 'usbpd' command
+void cli_usbpd_show_help(Cli *cli) {
+    cli_printf(cli, "Usage: " EOL);
+    cli_printf(cli, "\tpdo\t- Prints the last set of [Source] Power-Data-Objects (PDOs)" EOL);
+}
+static const CliUsbpdShowOption cli_usbpd_show_options[] = {
+    {.cb = cli_usbpd_show_pdo, .option = "pdo"},
+    {.cb = cli_usbpd_show_help, .option = "-h"},
+    {.cb = cli_usbpd_show_help, .option = "--help"},
+};
+static const size_t cli_usbpd_show_options_count = 
+    sizeof(cli_usbpd_show_options) / sizeof(cli_usbpd_show_options[0]);
+void cli_usbpd_show(Cli *cli, std::vector<std::string>& argv) {
+    if(argv.size() < 1) {
+	// Print help information
+	cli_usbpd_show_help(cli);
+	return;
+    }
+    cli_printf(cli, "TEST-remove" EOL);
+
+    for(size_t i = 0; i < cli_usbpd_show_options_count; i++) {
+	const CliUsbpdShowOption *cl = &cli_usbpd_show_options[i];
+	if(cl->option == argv[0]) {
+	    cl->cb(cli);
+	}
+    }
+    return;
+}
+
 void cli_usbpd_help(Cli *cli) {
     cli_printf(cli, "Usage: " EOL);
-    cli_printf(cli, "\t usbpd show	- Print state-machine parameters" EOL);
+    cli_printf(cli, "\tusbpd show\t- Print state-machine parameters" EOL);
 }
 static const CliUsbpdSubcommand cli_usbpd_subcommands[] = {
     {.fn = cli_usbpd_show, .name = "show"},
