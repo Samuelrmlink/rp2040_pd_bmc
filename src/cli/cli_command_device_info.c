b1d56252525e5d5f7f10556d00470e6e64559859
Samuel Hedrick <samuel@rml.ink>
2026-07-13 00:20:55 -0500

    cli 'device_info': Added GIT repo, PICO_PLATFORM, PICO_BOARD information. This data is collected by cmake before a build.

--- src/cli/cli_command_device_info.c
+++ src/cli/cli_command_device_info.c
@@ -4,11 +4,12 @@
 
 void cli_device_info(Cli* cli, const char* args) {
     (void)args;
-    cli_printf(cli, "github: https://github.com/Samuelrmlink/rp2040_pd_bmc\r\n");
+    cli_printf(cli, "git repo: %s\r\n", FW_GIT_REPO);
 
     char id[2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1];
     pico_get_unique_board_id_string(id, sizeof(id));
     cli_printf(cli, "platform: %s\r\n", FW_PLATFORM);
+    cli_printf(cli, "board: %s\r\n", PICO_BOARD);
     cli_printf(cli, "flash_id: %s\r\n", id);
     cli_printf(cli, "fw_build_date: %s\r\n", FW_BUILD_DATE);
     cli_printf(cli, "fw_git_commit: %s\r\n", FW_GIT_COMMIT);
